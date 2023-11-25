c4b53f28056f62dd580d4490718642b9ada88ca4
Hubert Elias <hubert.elias@otoma.com>
2023-11-25 14:32:04 +0100

    Hello world!

--- src/consts.ts
+++ src/consts.ts
@@ -4,27 +4,27 @@
  * A few of these are not used yet, and are subject to change, example of this is Author.
  */
 export const SITE_METADATA = {
-    title: 'Astro Starter Blog',
-    headerTitle: 'Astro Starter Blog',
-    description: 'A blog created with Astro and Tailwind.css',
+    title: "Hubert's Dev Journal",
+    headerTitle: "Hubert's Dev Journal",
+    description: 'My thoughts on building software',
     language: 'en-us',
     theme: 'system', // Options: system, light, dark, Does not work yet
-    siteUrl: 'https://astro-starter-blog.yon.fun/',
-    siteRepo: 'https://github.com/wanoo21/tailwind-astro-starting-blog',
+    siteUrl: 'https://github.com/hubelias/blog', // TODO
+    siteRepo: 'https://github.com/hubelias/blog',
     siteLogo: '/static/images/logo.png',
-    socialBanner: '/static/images/twitter-card.png',
+    // socialBanner: '/static/images/twitter-card.png',
     locale: 'en-US',
 
     // The following are subject to change. They are placeholders for now.
     // This project provide a default author content see src/content/authors/default.mdx, so these details are better to be inserted there.
-    author: 'John Doe',
-    mastodon: 'https://mastodon.social/@mastodonuser',
-    email: 'address@yoursite.com',
-    github: 'https://github.com',
-    twitter: 'https://twitter.com/Twitter',
-    facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com',
-    linkedin: 'https://www.linkedin.com',
+    author: 'Hubert Elias',
+    // mastodon: 'https://mastodon.social/@mastodonuser',
+    email: 'hubelias@gmail.com',
+    github: 'https://github.com/hubelias',
+    twitter: 'https://twitter.com/hub_elias',
+    // facebook: 'https://facebook.com',
+    // youtube: 'https://youtube.com',
+    linkedin: 'https://www.linkedin.com/in/hubelias',
 
     // These are not supported yet
     // analytics: {
@@ -106,6 +106,6 @@ export const NAVIGATION = [
     {href: '/', title: 'Home'},
     {href: '/blog', title: 'Blog'},
     {href: '/tags', title: 'Tags'},
-    {href: '/projects', title: 'Projects'},
-    {href: '/about', title: 'About'},
+    // {href: '/projects', title: 'Projects'},
+    {href: '/about', title: 'About me'},
 ]
